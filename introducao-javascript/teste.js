1ae1940b9cb166edff0a76a84cd57a5bf32459c6
nathoalmeida <nathalia.oalmeida@gmail.com>
2022-07-10 09:02:01 -0300

    testes intellij

---
+++ introducao-javascript/teste.js
@@ -0,0 +1,10 @@
+var firstName = "João" ;
+var lastName = 'Souza' ;
+
+if (firstName === 'João') {
+    var firstName = 'Pedro';
+    let lastName = 'Silva';
+}
+
+console.log (firstName, lastName) ;
+
